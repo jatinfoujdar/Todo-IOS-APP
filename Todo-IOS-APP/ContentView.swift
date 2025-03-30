02a514af278e3d81a9f573d3c1373e725f4c36d5
Jatin Foujdar <jatin321001@gmail.com>
2025-03-30 14:13:59 +0530

    Initial Commit

---
+++ Todo-IOS-APP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Todo-IOS-APP
+//
+//  Created by jatin foujdar on 30/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Todo-IOS-APP/Todo_IOS_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_IOS_APPApp.swift
+//  Todo-IOS-APP
+//
+//  Created by jatin foujdar on 30/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Todo_IOS_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
